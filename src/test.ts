376e449b6f2ed6452cda6c35ef0d9bb7f4650928
Alexandru Jercan <jercan_alex27@yahoo.com>
2020-10-14 10:45:47 +0300

    Created Package

---
+++ src/test.ts
@@ -0,0 +1,20 @@
+﻿import * as oracledbWrapper from './oracledbWrapper';
+import OracleDB from "oracledb";
+
+describe("Unit Tests", () => {
+    describe("Pool Tests", () => {
+        it("Should Create Pool If Credentials Are Valid", async () => {
+            const config: OracleDB.PoolAttributes = {
+                user: 'system',
+                password: 'parola',
+                connectString: 'localhost/orclpdb1',
+                poolMin: 1,
+                poolMax: 10,
+            };
+
+            const pool = await oracledbWrapper.createPool(config);
+
+            expect(pool).toBeDefined();
+        })
+    })
+})
